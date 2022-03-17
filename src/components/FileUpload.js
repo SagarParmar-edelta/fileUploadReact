d5642d5d5c7b56e0794cad02f409f668ae64fb53
tenzmetok <tenzin.metok@edeltaes.com>
2022-03-17 13:54:51 +0530

    feat/add-modal-popup

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
 import "./fileUpload.css";
 import Axios from "axios";
+import Example from "./modal";
 const FileUpload = () => {
   const [file, setFile] = useState({});
   const [uploadedFile, setUploadedFile] = useState({});
